479394a01d0b2a794b9866ac0ac712c903309653
William Wong <compulim@hotmail.com>
2020-05-25 23:55:53 -0700

    Add tests for 2 bundles

--- __tests__/dist.js
+++ __tests__/dist.js
@@ -10,8 +10,21 @@ async function waitOnce(eventTarget, name) {
   return new Promise(resolve => eventTarget.addEventListener(name, resolve, { once: true }));
 }
 
-test('Able to load via window.simpleUpdateIn', async () => {
-  const { window } = await JSDOM.fromFile(join(__dirname, 'dist.html'), {
+test('Able to load development bundle via window.simpleUpdateIn', async () => {
+  const { window } = await JSDOM.fromFile(join(__dirname, 'dist.development.html'), {
+    resources: 'usable',
+    runScripts: 'dangerously'
+  });
+
+  await waitOnce(window, 'load');
+
+  const actual = window.simpleUpdateIn({ one: 1, two: 2 }, ['three'], () => 3);
+
+  expect(actual).toEqual({ one: 1, two: 2, three: 3 });
+});
+
+test('Able to load production bundle via window.simpleUpdateIn', async () => {
+  const { window } = await JSDOM.fromFile(join(__dirname, 'dist.production.html'), {
     resources: 'usable',
     runScripts: 'dangerously'
   });
